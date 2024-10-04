081f00479671f6941a57f5c2f24245ebdfcbde98
Adam Lamont <adam_lamont@wrdsb.ca>
2024-10-04 09:56:44 -0400

    added mouse clicking to draw rectangles and ArrayList

--- DrawingExample/DrawExample.java
+++ DrawingExample/DrawExample.java
@@ -2,6 +2,7 @@ package DrawingExample;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -20,12 +21,13 @@ public class DrawExample extends JComponent implements MouseMotionListener, Mous
     private BasicStroke normalLine = new BasicStroke(1);
     private int mouseX = 0;
     private int mouseY = 0;
+    private ArrayList<Rectangle> rectangles = new ArrayList<>();
 
     // constructor to make the window
     public DrawExample(){
         // make the main window
-        JFrame frame = new JFrame("My Game");
-        // make sure you can close the window
+        JFrame frame = new JFrame("My game");        // change to the screen has happened
+       
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // show the window
         frame.setVisible(true);
@@ -52,9 +54,38 @@ public class DrawExample extends JComponent implements MouseMotionListener, Mous
         Graphics2D g2d = (Graphics2D)g;
         // clear the window
         g.clearRect(0, 0, WIDTH, HEIGHT);
+        // set colour to draw with
         g.setColor(Color.BLACK);
+        // adjust outline size
         g2d.setStroke(this.thickerLine);
+        // drawing a rectangle
         g.drawRect(mouseX, mouseY, 50, 20);
+
+        // draw all stamped retangles
+        for(int i = 0; i < rectangles.size(); i++){
+            // take a rectangle from the list
+            Rectangle r = rectangles.get(i);
+            g.setColor(Color.RED);
+            g.fillRect(r.x, r.y, r.width, r.height);
+        }
+    }
+
+
+    public void run(){
+        // game loop
+        while(true){
+            // do the things you need to do
+
+
+            // update the screen
+            repaint();
+            // wait a little bit
+            try{
+                Thread.sleep(10);
+            }catch(Exception e){
+
+            }
+        }
     }
 
 
@@ -63,26 +94,32 @@ public class DrawExample extends JComponent implements MouseMotionListener, Mous
 
     public static void main(String[] args) {
         DrawExample window = new DrawExample();
-
+        // start the game loop
+        window.run();
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        
+        // make rectangle where I click
+        Rectangle r = new Rectangle(e.getX(), e.getY(), 50, 20);
+        // add it to the rectangle list
+        rectangles.add(r);
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
         this.mouseX = e.getX();
         this.mouseY = e.getY();
-        // change to the screen has happened
-        // redraw it
-        repaint();
+
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        
+        // make rectangle where I click
+        Rectangle r = new Rectangle(e.getX(), e.getY(), 50, 20);
+        // add it to the rectangle list
+        rectangles.add(r);
+        System.out.println(rectangles.size());
     }
 
     @Override
