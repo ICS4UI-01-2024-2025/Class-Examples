a228dae7154ab6ecf4008eabb757f906be28719b
Mr. Lamont <adam_lamont@wrdsb.ca>
2024-11-13 08:42:04 -0500

    Updated Queens to be more efficient

--- EightQueens.java
+++ EightQueens.java
@@ -7,25 +7,27 @@ public class EightQueens {
         if (queens.size() == n) {
             return true;
         }
-        // find a safe spot on the board
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (isSafe(row, col, queens)) {
-                    // make the queen at this spot
-                    Queen q = new Queen(row, col);
-                    queens.add(q);
-                    boolean answer = placeQueens(n, queens);
-                    // did the answer fully work?
-                    if (answer) {
-                        return true;
-                    } else {
-                        // this spot didn't pan out
-                        // remove the queen and keep trying
-                        queens.remove(queens.size() - 1);
-                    }
+        // notice that each queen MUST be on their own row
+        // assume the next empty row
+        int row = queens.size();
+        // find a safe spot in that row
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, queens)) {
+                // make the queen at this spot
+                Queen q = new Queen(row, col);
+                queens.add(q);
+                boolean answer = placeQueens(n, queens);
+                // did the answer fully work?
+                if (answer) {
+                    return true;
+                } else {
+                    // this spot didn't pan out
+                    // remove the queen and keep trying
+                    queens.remove(queens.size() - 1);
                 }
             }
         }
+        
 
         // wnet through all spots, and couldn't find one
         return false;
@@ -58,13 +60,21 @@ public class EightQueens {
         EightQueens test = new EightQueens();
         ArrayList<Queen> solution = new ArrayList<>();
 
-        boolean found = test.placeQueens(15,solution);
+        
+        long startTime = System.currentTimeMillis();
+        boolean found = test.placeQueens(30,solution);
         if(found){
             // print solution
             for (int i = 0; i < solution.size(); i++) {
                 Queen q = solution.get(i);
                 System.out.println(q.getRow() + ", " + q.getCol());
             }
+        }else{
+            System.out.println("No solutions found");
         }
+
+        long elapsed = (System.currentTimeMillis() - startTime)/1000;
+        System.out.println("Time Taken: " + elapsed + "s");
+
     }
 }
